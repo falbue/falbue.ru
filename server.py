7de5353a7ba490adfbff1f12f3e5bd3c6f146293
falbue <cyansair05@gmail.com>
2025-01-04 17:23:14 +0500

    указан правильный порт

--- server.py
+++ server.py
@@ -22,5 +22,5 @@ def get_projects():
     
     return jsonify(projects)
 
-# app.run(host='0.0.0.0', port=80)
-app.run(host='0.0.0.0',debug=True)
+app.run(host='0.0.0.0', port=80)
+# app.run(host='0.0.0.0',debug=True)
